bb712d9b53e7ee241086a567114cb81f81a27759
Jose <qjoseantonio@gmail.com>
2023-11-28 23:14:01 -0800

    title,author and url display

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import './App.css'
-import bookData from './book-data.json'
+import "./App.css";
+import bookData from "./book-data.json";
 
 function App() {
   return (
-    <div>
-      <h1>Freeshelf</h1>
+    
+      
+      <div className="App">
+      
+      {
+        bookData.map( bookdata => {
+          return(
+            <div className="box">
+             <h1>{ bookdata.title }</h1>
+             { bookdata.author }
+             <p></p>
+             { bookdata.url }
+
+            </div>
+          )
+        })
+      }
+
+      
+      
+      
+      
       {/* Use bookData to show books on the page*/}
     </div>
   )
 }
+  
+   
 
-export default App
+export default App;
